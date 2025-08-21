df53faa9dc7dfef1b7ef5b1063b1f8e7303ce198
Catalin Tamas <ctamas@zerobias.com>
2025-08-21 12:37:14 +0300

    ci: update publish script to not commit the generated readme

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-git add .
+git add . && git reset HEAD README.md 2>/dev/null || true
 git commit -m "chore(release): bump version"
 npx lerna list --since --ndjson | jq -r '(.name) + "@" + (.version)' | xargs -L1 git tag -a -m "chore(release): bump version"
 git push origin

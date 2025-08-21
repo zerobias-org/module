68fbbe11540dfc4e2da9c899a28855f2f2a25208
Catalin Tamas <ctamas@zerobias.com>
2025-08-21 12:47:55 +0300

    ci: fix readme push again

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
-git add . && git reset HEAD README.md 2>/dev/null || true
+git add .
+find . -name "README.md" -exec git reset --staged {} \; 2>/dev/null || true
 git commit -m "chore(release): bump version"
 npx lerna list --since --ndjson | jq -r '(.name) + "@" + (.version)' | xargs -L1 git tag -a -m "chore(release): bump version"
 git push origin

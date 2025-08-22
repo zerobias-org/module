910c7e6757bcd2c85972574f274d7e8b166a91ac
Catalin Tamas <ctamas@zerobias.com>
2025-08-22 15:15:25 +0300

    ci: make sure we don't commit the generated readme

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 git add .
-find . -name "README.md" -exec git reset --staged {} \; 2>/dev/null || true
+git restore --staged "*README.md"
 git commit -m "chore(release): bump version"
 npx lerna list --since --ndjson | jq -r '(.name) + "@" + (.version)' | xargs -L1 git tag -a -m "chore(release): bump version"
 git push origin
